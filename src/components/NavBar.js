67321e7356104b6bf1f8433bbbc421515ff3cc64
Bobby Bates <robertfbatesjr@gmail.com>
2022-06-19 09:16:01 -0400

    add NavLinks to NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,14 @@
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <>
+      <NavLink exact to='/'>Home</NavLink>
+      <NavLink to='/movies'>Movies</NavLink>
+      <NavLink to='/directors'>Directors</NavLink>
+      <NavLink to='/actors'>Actors</NavLink>
+    </>
+  )
 }
 
 export default NavBar;
