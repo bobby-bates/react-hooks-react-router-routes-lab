f0117a1bfe8eae9a28474ff90ffb65cf473eaf4d
Bobby Bates <robertfbatesjr@gmail.com>
2022-06-19 09:41:30 -0400

    add movie list

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,7 +1,23 @@
 import { movies } from "../data";
 
 function Movies() {
-  return <div>Hi from Movies</div>;
+  const movieMaker = movies.map(({ title, time, genres }) => (
+    <div key={[title, time].join()}>
+      <h2>{title}</h2>
+      <h3>Runtime: {time} minutes</h3>
+      <h4>Genres:</h4>
+      <ul>
+        {genres.map(genre => <li key={[title, genre].join()}>{genre}</li>)}
+      </ul>
+    </div>
+  ))
+
+  return (
+    <>
+      <h1>Movies Page</h1>
+      {movieMaker}
+    </>
+    )
 }
 
 export default Movies;
